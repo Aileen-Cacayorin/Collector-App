d82d5aecf8dc6353ff7375f241565c7f79d7f41f
AileenBull <aileen.cacayorin@gmail.com>
2020-02-08 21:32:22 -0600

    Initial Commit

---
+++ Collector App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collector App
+//
+//  Created by Aileen Bull on 2/8/20.
+//  Copyright © 2020 Aileen Bull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
